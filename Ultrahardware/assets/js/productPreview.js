060f5268ec1f99bb1900b87c1c45c20698bf909a
AndresGroselj <61983866+AndresGroseljDuoc@users.noreply.github.com>
2020-09-17 14:32:59 -0300

    productPreview cabio de imagenes

--- Ultrahardware/assets/js/productPreview.js
+++ Ultrahardware/assets/js/productPreview.js
@@ -6,7 +6,17 @@ $( document ).ready(function() {
     });
 });
 
-function displayPreview(){
+function displayPreview(img_id){
+
+    $('#product-preview-images').children('img').each(function () {
+        this.classList.remove("d-block");
+        this.classList.add("d-none");
+        if (this.getAttribute("for") == img_id){
+            this.classList.remove("d-none");
+            this.classList.add("d-block");
+        }
+    });
+
     product_preview.classList.remove("d-none")
     product_preview.classList.add("d-block")
 }

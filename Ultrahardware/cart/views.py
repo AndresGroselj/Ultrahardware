ffd452a64e24af16fa07fa12234d1475259ef745
AndresGroselj <61983866+AndresGroseljDuoc@users.noreply.github.com>
2020-09-19 21:09:17 -0300

    arreglos responsivos

--- Ultrahardware/cart/views.py
+++ Ultrahardware/cart/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from products import models as product_models
 
 # Create your views here.
 
 def Cart(request):
+    category_parents = product_models.Category_parent.objects.order_by("order")
     context = {
+        "category_parents": category_parents
     }
     return render(request, "cart/Cart.html", context)

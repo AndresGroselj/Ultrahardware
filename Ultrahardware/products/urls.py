430387d648baba9105b10804d1b8c6dbfc74bd66
Andres Groselj <an.groselj@alumnos.duoc.cl>
2020-11-04 23:18:12 -0300

    url unit test

--- Ultrahardware/products/urls.py
+++ Ultrahardware/products/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 app_name = "products"
 
 urlpatterns = [
-    path("popular/", views.Popular, name="popular"),
     path("<int:_product_id>/", views.Product, name="product"),
+    path("popular/", views.Popular, name="popular"),
     path("list/", views.List, name="list"),
     path("list/<int:_product_id>", views.Edit, name="edit"),
     path("list/add", views.Add, name="add"),

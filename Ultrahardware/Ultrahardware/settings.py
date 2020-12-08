9bc72c60684d657396c80ddee1f2875fd1deeaef
Andres Groselj <an.groselj@alumnos.duoc.cl>
2020-12-07 22:52:52 -0300

    configuracion manifest e imagenes icono y spash

--- Ultrahardware/Ultrahardware/settings.py
+++ Ultrahardware/Ultrahardware/settings.py
@@ -169,3 +169,36 @@ REST_FRAMEWORK = {
 LOGIN_REDIRECT_URL = "/"
 
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'serviceworker.js')
+
+
+#configuracion del manifest PWA
+PWA_APP_NAME = 'Ultrahardware'
+PWA_APP_DESCRIPTION = "Tienda de elecronica al mejor precio con sede en la region metropolitana"
+PWA_APP_THEME_COLOR = '#32e0c4'
+PWA_APP_BACKGROUND_COLOR = '#eeeeee'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/img/logo_160x160.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/img/logo_160x160.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/img/splash_640x1136.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'es'
+PWA_APP_DEBUG_MODE = True

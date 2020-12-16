fba8d62bd28ffb11af9d2ef3f550c3ecb4732d02
Andres Groselj <an.groselj@alumnos.duoc.cl>
2020-12-16 00:46:44 -0300

    cambio almacenamiento infoProductos y id html producto en carrito
    infoProductos: array -> json
    elemento producto en carrito: id producto en id elemento

--- Ultrahardware/assets/controllers/cartController.js
+++ Ultrahardware/assets/controllers/cartController.js
@@ -1,9 +1,10 @@
 var container = null;
-var infoProducts = [];
+var infoProducts = {};
 
 $(document).ready(function(){
     container = $("#cart-products");
     getProducts();
+    PrintProducts();
 });
 
 function getProducts(){
@@ -16,7 +17,7 @@ function getProducts(){
             url: `/api/productcard/${productId}/`,
             dataType: 'json',
             success: function(response){
-                console.log(response);
+                //console.log(response);
                 infoProducts[response["product_id"]] = response; 
                 infoProducts[response["product_id"]]["quantity"] = quantity;
             },
@@ -28,23 +29,38 @@ function getProducts(){
     });
 }
 
-function CreateProductCard(product, quantity){
+function PrintProducts(){
+    $.each(infoProducts, function(index, product){
+        var card = CreateProductCard(product)
+        appendCard(card);
+    });
+}
+
+function CreateProductCard(product){
     /*
     <div class="product-item card">
-        <input type="hidden" value="1" id="productid" name="productid">
-        <div class="card-block">
-        <div class="row">
-            <div class="col-lg-1 col-2">
-                <img src="https://www.pcfactory.cl/public/foto/38367/1_500.jpg?t=1597845348" alt="telefono-samsung" class="product-image">
+            <input type="hidden" value="1" id="productid" name="productid">
+            <div class="card-block">
+                <div class="row">
+                    <div class="col-lg-2 col-3">
+                        <img src="https://www.pcfactory.cl/public/foto/38367/1_500.jpg?t=1597845348" alt="telefono-samsung" class="product-image">
+                    </div>
+                    <div class="col-lg-10 col-9">
+                        <div class="row">
+                            <div class="col-lg-6 col-10 mr-auto">Telefono Samsung</div>
+                            <img src="/static/img/red_trashcan.png" alt="X" class="removeButton">
+                        </div>
+                        <div class="row justify-content-between">
+                            <div class="col-lg-5 col-9 text-left"><span>$500.000</span> <span class="descuento">descuento</span></div>
+                            <div class="col-lg-2 col-3 ammount">
+                                <input type="number" class="form-control" value="2" max="20" min="1">
+                            </div>
+                            <div class="col-lg-3 col-9 text-right">$1.000.000</div>
+                        </div>
+                    </div> 
+                </div>
             </div>
-            <div class="col-lg-6 col-10 text-center">Telefono Samsung</div>
-            <div class="col-lg-2 col-3 ammount">
-                <input type="number" class="form-control" value="2" max="20" min="1">
-            </div>
-            <div class="col-lg-3 col-9 text-right">$1.000.000</div>
         </div>
-        </div>
-    </div>
     */
 
     var card = $("<div></div>");
@@ -59,7 +75,7 @@ function appendCard(card){
 }
 
 function emptyContainer(){
-    container.empty();
+    //container.empty();
 }
 
 function removeProduct(id){

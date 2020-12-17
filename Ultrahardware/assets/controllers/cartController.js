28a94d4b422d38f9cfd890a3acd9ca05d1719287
Andres Groselj <an.groselj@alumnos.duoc.cl>
2020-12-17 03:49:24 -0300

    mejoras elemento producto en carrito y creacion del mismo

--- Ultrahardware/assets/controllers/cartController.js
+++ Ultrahardware/assets/controllers/cartController.js
@@ -42,34 +42,44 @@ function printProducts(){
 
 function CreateProductCard(product){
     /*
-    <div class="product-item card">
-            <input type="hidden" value="1" id="productid" name="productid">
-            <div class="card-block">
-                <div class="row">
-                    <div class="col-lg-2 col-3">
-                        <img src="https://www.pcfactory.cl/public/foto/38367/1_500.jpg?t=1597845348" alt="telefono-samsung" class="product-image">
-                    </div>
-                    <div class="col-lg-10 col-9">
-                        <div class="row">
-                            <div class="col-lg-6 col-10 mr-auto">Telefono Samsung</div>
-                            <img src="/static/img/red_trashcan.png" alt="X" class="removeButton">
-                        </div>
-                        <div class="row justify-content-between">
-                            <div class="col-lg-5 col-9 text-left"><span>$500.000</span> <span class="descuento">descuento</span></div>
-                            <div class="col-lg-2 col-3 ammount">
-                                <input type="number" class="form-control" value="2" max="20" min="1">
-                            </div>
-                            <div class="col-lg-3 col-9 text-right">$1.000.000</div>
-                        </div>
-                    </div> 
-                </div>
+    <div class="product-item card" id="product-1">
+        <div class="row">
+            <div class="col-lg-2 col-3">
+                <img src="https://www.pcfactory.cl/public/foto/38367/1_500.jpg?t=1597845348" alt="telefono-samsung" class="product-image">
             </div>
+            <div class="col-lg-10 col-9">
+                <div class="row">
+                    <div class="col-lg-6 col-10 mr-auto">Telefono Samsung</div>
+                    <img src="/static/img/red_trashcan.png" alt="X" class="removeButton">
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-lg-5 col-9 text-left"><span>$500.000</span> <span class="descuento">descuento</span></div>
+                    <div class="col-lg-2 col-3 ammount">
+                        <input type="number" class="form-control" value="2" max="20" min="1" id="product-quantity-1">
+                    </div>
+                    <div class="col-lg-3 col-9 text-right" id="product-total-1">$1.000.000</div>
+                </div>
+            </div> 
         </div>
+    </div>
     */
 
     var card = $("<div></div>");
     card.addClass("product-item card");
-    card.text(product["nombre"]);
+
+    var row = $("<div></div>");
+    row.addClass("row");
+
+    var imageContainer = $("<div></div>");
+    imageContainer.addClass("col-lg-2 col-3");
+
+    var image = $("<img></img>");
+    image.addClass("product-image");
+    image.attr("src", product["imagen_principal"]);
+
+    imageContainer.append(image);
+    row.append(imageContainer);
+    card.append(row);
 
     return card;
 }
